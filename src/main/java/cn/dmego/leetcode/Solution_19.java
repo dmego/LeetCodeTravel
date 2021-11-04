1527f5647ac9e623f96af97423aeba751c63d095
dmego <dmeago@gmail.com>
2021-11-04 12:27:16 +0800

    solution 19、367

---
+++ src/main/java/cn/dmego/leetcode/Solution_19.java
@@ -0,0 +1,20 @@
+package cn.dmego.leetcode;
+
+import cn.dmego.leetcode.util.ListNode;
+
+/**
+ 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+ 进阶：你能尝试使用一趟扫描实现吗？
+ */
+public class Solution_19 {
+
+    /**
+
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        return null;
+    }
+
+}
