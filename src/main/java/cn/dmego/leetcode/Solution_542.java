1198de3d0f2b42538fce7da2f30151edff16de00
dmego <dmeago@gmail.com>
2021-12-28 11:20:12 +0800

    Solution 542/784/746

---
+++ src/main/java/cn/dmego/leetcode/Solution_542.java
@@ -0,0 +1,61 @@
+package cn.dmego.leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author dmego
+ * @date 2021/12/22 20:58
+ */
+public class Solution_542 {
+    /**
+     多源 BFS: 求 每个 1 到最近一个 0 的距离 --> 转换为 0 到 每一个 1 的最近距离
+     为什么要转换: 由 0 去找 1，在找的过程中计算距离；如果是 1 找 0， 找到后，再回去更新 1 的 值就比较麻烦了
+     初始化时，把 0 入队列，把所有 1 标记为 未被访问过 -1
+     */
+    public int[][] updateMatrix(int[][] mat) {
+        Deque<int[]> deque = new ArrayDeque<>();
+        int[][] direction = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
+        int m = mat.length;
+        int n = mat[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    // 0 入队列
+                    deque.addLast(new int[]{i, j});
+                } else if (mat[i][j] == 1){
+                    // 在原数组基础上，将所有 1 初始化为未访问状态 -1
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        int step = 0; // 0 ~ 1 的最小距离 (例比994: 腐烂橘子感染最近一个新鲜橘子的时间)
+        while (!deque.isEmpty()) {
+            step++; // 最小距离也相当于 BFS 的层数
+            int size = deque.size();
+            for (int i = 0; i < size; i++) {
+                int[] node = deque.pollFirst();
+                for (int[] direct : direction) {
+                    int x = node[0] + direct[0];
+                    int y = node[1] + direct[1];
+                    // 如果在矩阵内，并且是未标识的 1 （-1）
+                    if (inArea(mat, x, y) && mat[x][y] == -1) {
+                        // 这个未标识 1 到 最近 0 的距离，也就是 0 到 这个 1 的最近距离，也就是当前遍历的层数
+                        // 在原数组基础上进行修改，mat[x][y] 原值 -1， 改完 step，也就是距离最近0的距离
+                        mat[x][y] = step;
+                        // 将这个已经标识的 1 放入队列(类比994：将被感染的橘子放入队列)
+                        // 也就是下一层需要遍历的节点
+                        deque.addLast(new int[]{x, y});
+                    }
+                }
+            }
+        }
+
+        return mat;
+    }
+
+    public boolean inArea(int[][] mat, int r, int c) {
+        return 0 <= r && r < mat.length && 0 <= c && c < mat[0].length;
+    }
+}
---
+++ src/main/java/cn/dmego/leetcode/Solution_746.java
@@ -0,0 +1,62 @@
+package cn.dmego.leetcode;
+
+/**
+ * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
+ *
+ * 你可以选择从下标为 0 或下标为 1 的台阶开始爬楼梯。
+ *
+ * 请你计算并返回达到楼梯顶部的最低花费。
+ */
+public class Solution_746 {
+    /**
+     理解题意：
+     - cost[i] 表示从 第 i 个台阶往上爬需要支付的费用，而支付之后，可以选择往上爬一个台阶，或两个台阶
+     - 我们可以选择从下标为 0 的台阶，或者 下标为 1 的台阶开始往上爬
+     - cost.length >= 2,
+
+     dp[i] 的含义：
+        从第 i 阶爬到楼顶的最小花费（i 从 0 开始）
+        因为可以选择从下标 0 或 下标 1 的台阶开始爬楼梯，
+
+     初始化值： 因为台阶最小为 2 阶
+        dp[0] = cost[0] （从 0 开始往上爬 2 阶到楼顶，花费 cost[0]）
+        dp[1] = cost[1] (从 1 开始往上爬 1 阶到楼顶，花费 cost[1])
+
+     状态转移方程：dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+        解释：min(dp[i - 1], dp[i - 2]) 表示跳到 i 台阶的最小花费，因为调到 i 台阶可以从 i-1开始跳1阶，也可以从 i-2 开始跳2阶
+            + cost[i] 表示从 i 台阶开始往上跳需要的花费，不管是跳到 i+1 还是 i+2 都需要 cost[i] 费用
+
+     理解到达顶楼：
+        cost[cost.length - 1] 是最后一个台阶，如果从这个台阶往上跳 1 阶就能跳到顶楼
+        cost[cost.length - 2] 是倒数第二个台阶，如果从这个台阶往上跳 2 阶就能到顶楼
+     所以 跳到顶楼的最小花费是 Math.min(dp[cost.length-1], dp[cost.length-2])
+
+     */
+    public static int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i];
+            System.out.print(dp[i] + " ");
+        }
+
+        return Math.min(dp[cost.length - 1], dp[cost.length -2]);
+    }
+
+    public static int minCostClimbingStairs2(int[] cost) {
+        int a = cost[0], b = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            if(i % 2 == 0) a = Math.min(a, b) + cost[i];
+            else b = Math.min(a, b) + cost[i];
+        }
+        return Math.min(a, b);
+    }
+
+
+    public static void main(String[] args) {
+        int[] cost = new int[]{10,15,20};
+        System.out.println(minCostClimbingStairs(cost));
+    }
+
+}
