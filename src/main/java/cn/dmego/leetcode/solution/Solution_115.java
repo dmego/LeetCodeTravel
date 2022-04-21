93c7e9fcfabe2100ec65a4664e0a1d0fa711a0e6
Dmego <dmeago@gmail.com>
2022-04-21 11:12:13 +0800

    add some Solution

---
+++ src/main/java/cn/dmego/leetcode/solution/Solution_115.java
@@ -0,0 +1,45 @@
+package cn.dmego.leetcode.solution;
+
+import java.util.Arrays;
+
+/**
+ * 不同的子序列
+ * @author dmego
+ * @date 2022/04/21 10:20
+ */
+public class Solution_115 {
+
+    /*
+    1. dp 数组的定义: dp[i][j] 表示 以下标 i-1 结尾 s 的子序列中，以下标 j-1 结尾的 t 出现的个数
+    2. 初始化：
+        dp[i][0] 表示以下标 i-1 结尾 s 的子序列中，空串出现的个数，很明显都是 1，所以 dp[i][0] = 1;
+        dp[0][j] 表示空串的子序列中，以下标 j-1 结尾的 t 出现的个数，当 j = 0 时，空串是空串的子序列，dp[0][0] = 1,
+        否则，dp[0][j] = 0, 空串的子序列只有空串。
+    3. 递推公式：
+        当 s[i - 1] = t[j - 1] 时，说明 s 在下标 i-1 位置的字符和 t 在下标 j-1 位置的字符相同，此时分为两种情况：
+            使用 s[i - 1] 参与匹配子序列：dp[i][j] = dp[i - 1][j - 1]:
+                因为 s[i-1] 和 t[j-1]能匹配上，t (0~j-1) 出现的个数和 以下标i-2结尾s的子序列中，以下标j-2结尾的t出现的个数相同
+            不使用 s[i - 1] 参与匹配子序列：dp[i][j] = dp[i - 1][j]：使用 s[i-1] 的前一个s[i-2] 的结果，
+
+        当 s[i - 1] != t[j - 1]时，不能使用 s[i - 1] 参与匹配子串，dp[i][j] = dp[i - 1][j]
+    4. 遍历顺序，从前往后
+    5. 结果：最后返回 dp[s.length()][t.length()]
+    */
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+        Arrays.fill(dp[0], 0);
+        for (int i = 0; i <= s.length(); i++)
+            dp[i][0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+
+}
---
+++ src/main/java/cn/dmego/leetcode/solution/Solution_583.java
@@ -0,0 +1,64 @@
+package cn.dmego.leetcode.solution;
+
+/**
+ * 两个字符串的删除操作
+ * @author dmego
+ * @date 2022/04/21 10:34
+ */
+public class Solution_583 {
+
+    /**
+    解法一：最长公共子序列的思路
+    求两个字符串要想达到相同，所需要进行删除的最少字符数
+    换一种思路就是，求出两个字符串的最长公共子序列的长度 len，这个长度就是最终两个字符串做删除之后达到相同时的长度
+    那么需要删除的字符数就是两个字符串的长度分别减去 len ，然后相加就是结果
+    */
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        int len = dp[word1.length()][word2.length()];
+        return (word1.length() - len) + (word2.length() - len);
+    }
+
+    /**
+    解法二：不同的子序列思路
+    1. dp 数组的定义：dp[i][j] 表示以下标 i-1 结尾的 word1 和 以下标 j-1 结尾的 word2 要达到相等，需要进行最少的删除操作
+    2. 初始化：
+        dp[i][0] 表示下标为 i-1 结尾的 word1 成为 空串要进行的最少删除操作，dp[i][0] = i, 都删除后就是空串。
+        dp[0][j] 同样的道理，dp[0][j] = j
+    3. 递推公式：
+        当 word1[i-1] = word2[j-1] 时，不需要进行删除，dp[i][j] = dp[i - 1][j - 1];
+        当 word1[i-1] != word2[j-1] 时，有 3 种情况：
+            删除 word1[i-1]: dp[i][j] = dp[i - 1][j] + 1
+            删除 word2[j-1]: dp[i][j] = dp[i][j - 1] + 1
+            word1[i-1] 和 word2[j-1] 都删除：dp[i][j] = dp[i - 1][j - 1] + 2
+            要求最小值，去这三种情况的最小值即可
+    */
+    public int minDistance2(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= word2.length(); j++)
+            dp[0][j] = j;
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 2);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+
+    }
+
+}
