43a24f35b42a7086f3de66a4d17d5645c329ad12
Dmego <dmeago@gmail.com>
2022-11-07 23:29:13 +0800

    fix

--- src/main/java/cn/dmego/leetcode/solution/lc51_lc100/Solution_64.java
+++ src/main/java/cn/dmego/leetcode/solution/lc51_lc100/Solution_64.java
@@ -8,21 +8,30 @@ package cn.dmego.leetcode.solution.lc51_lc100;
 public class Solution_64 {
 
     /*
-    dp[i][j] 表示从 (i, j) 到 (m, n) 终点的最小路径和
-    我们从下到上，从右往左遍历
+    dp 数组定义：dp[i][j] 表示从 (i, j) 到 (m, n) 终点的最小路径和
+    dp 数组初始化：第一行与最左列的最小路径和可以直接求出来，因为只有一条路径到达
+    递推公式：dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+
     */
     public int minPathSum(int[][] grid) {
+        // dp[i][j] 表示坐标 (0, 0) 到坐标(i, j) 最小路径和
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m][n];
-        dp[m - 1][n - 1] = grid[m - 1][n - 1];
-        for (int i = m - 2; i >= 0; i--) dp[i][n - 1] = dp[i + 1][n - 1] + grid[i][n - 1];
-        for (int j = n - 2; j >= 0; j--) dp[m - 1][j] = dp[m - 1][j + 1] + grid[m - 1][j];
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+        dp[0][0] = grid[0][0];
+        // 初始化(0, 0) 到 (m - 1, 0) 也就是最上一行的最小路径值，只有一条路径，就是往右走
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        // 初始化(0, 0) 到 (0, n - 1) 也就是最左一列的最小路径值，也只有一条路径，就是往下走
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[0][0];
+        return dp[m - 1][n - 1];
     }
 }
